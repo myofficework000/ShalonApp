e8193a0def8b033f50eaf9e8a0751c79056f509b
PavaniVelma <pavanireddyvelma@gmail.com>
2023-09-07 17:43:50 -0400

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9"
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -47,6 +50,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
@@ -66,4 +72,46 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+    //Room Dependency
+    implementation("androidx.room:room-runtime:2.5.2")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+    //Dagger-hilt
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // ktx activity with view model injection
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    // For Observing the state of Livedata in Compose
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
+
+//    Coin
+    implementation("io.coil-kt:coil-compose:1.3.2")
+
+    // navigation component
+    implementation("androidx.navigation:navigation-compose:2.5.0")
+
+    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.1")
+
+
+    //Rx-Java
+    implementation("io.reactivex.rxjava3:rxjava:3.1.6")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("com.github.akarnokd:rxjava3-retrofit-adapter:3.0.0")
+
+    //for viewModel scope
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 }
