4c737fd232f7de24a3e1eecf97a03daea90f0620
PavaniVelma <pavanireddyvelma@gmail.com>
2023-09-08 13:37:28 -0400

    added SplashScreen, register screen and LoginScreen

--- app/src/main/java/com/example/shaloonapp/view/screens/SplashScreen.kt
+++ app/src/main/java/com/example/shaloonapp/view/screens/SplashScreen.kt
@@ -2,42 +2,111 @@ package com.example.shaloonapp.view.screens
 
 import android.window.SplashScreen
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.shaloonapp.R
+import com.example.shaloonapp.ui.theme.Purple40
+import com.example.shaloonapp.ui.theme.Yellow
 
 
-@Preview
 @Composable
-fun SplashScreen(){
+fun SplashScreen(navController: NavController){
 
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Purple40),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ){
 
         val rawComposition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.animation))
 
+        Text(
+            modifier = Modifier
+                .padding(top = 50.dp, start = 10.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineMedium,
+
+            text = stringResource(id = R.string.Lets_Get_Started)
+
+        )
+
         LottieAnimation(
             composition = rawComposition,
             modifier = Modifier
-                .size(500.dp),
+                .size(500.dp)
+                .padding(top = 50.dp)
+            ,
             contentScale = ContentScale.Inside,
             iterations = Int.MAX_VALUE
         )
+
+        Button(
+            onClick = {
+                TODO()
+            },
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(Yellow),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.SignUp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
+            )
+        }
+
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, start = 10.dp),
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodySmall,
+                text = stringResource(id = R.string.Aleardy_have_an_account_Login)
+            )
+        }
+
+
+
     }
 }
